cc429ea1827881f6ca7c3b2260a3eb9a57469f4f
Doğa Özkaraca <doga.ozkaraca@gmail.com>
2016-09-01 15:26:36 +0300

    Database password warning has been added.

--- Website/dbconfig.php
+++ Website/dbconfig.php
@@ -1,7 +1,9 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'dogaozka_bitpopW');    // DB username
+// TO-DO : db password will be changed after project launch
 define('DB_PASSWORD', '#Do2981@wq.q!');    // DB password
+// TO-DO : db password will be changed after project launch
 define('DB_DATABASE', 'dogaozka_bitpops');      // DB name
 //$connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
 //$database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
